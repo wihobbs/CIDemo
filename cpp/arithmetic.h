258091433f72fcff9c19302ba04e7da946c87c9c
William Hobbs <13199119+wihobbs@users.noreply.github.com>
2023-02-28 11:13:21 -0500

    cpp: add arithmetic library in C++

---
+++ cpp/arithmetic.h
@@ -0,0 +1,11 @@
+// This is a header file.
+// Header files are where classes are defined,
+// and then you fill in what they actually do in .cc files
+
+class Arithmetic {
+  public:
+    int addition(int x, int y);
+    int subtraction(int x, int y);
+    int multiplication(int x, int y);
+    int division(int x, int y);
+};
